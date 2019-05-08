b0a6bdca902f73151ded6c2d0edb4d443919e8f4
mmc <mmc@mmc.com>
2019-05-08 13:30:31 +0800

    初次提交

---
+++ file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+
+import subprocess
+
+def is_in_profile(file, content):
+    reader = open(file)
+
+    line = reader.readline()
+
+    while line != '' and line != None:
+        if -1 != line.find(content):
+            print(line)
+            reader.close()
+            return True
+        line = reader.readline()
+    reader.close()
+    return False
+
+def set_to_profile(cmd):
+    path = os.path.expanduser('~')
+    profile = path + '/' + '.profile'
+
+    is_existed = is_in_profile(profile, cmd)
+    if False == is_existed:
+        os.system("echo '' >> ~/.profile")
+        profile_cmd = "echo '%s' >> ~/.profile" % cmd
+        os.system(profile_cmd)
